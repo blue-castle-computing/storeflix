8e82fa98d6c4924457d2e7e1cb154a5900b803ef
blue-castle-computing <justin+blue-castle-computing@users.noreply.github.com>
2021-07-08 15:32:56 +0100

    Add logout

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -7,12 +7,22 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user
+  // sign up the user - asyncronous, returns a promise of a credential handled in .then
+  // password > 6 chars. When signed up, the user can be seen in the firebase console authentication
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
     console.log(cred.user);
-    // close the signup modal & reset form
+    // close the signup modal & reset form - get the id '#modal-signup from index.html modal id
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
 });
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+ });
